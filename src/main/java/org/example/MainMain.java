9b76661c0f9d543748665605d07f84d549d1f580
Tsumi <141481121+Tsumich@users.noreply.github.com>
2026-09-23 19:10:54 +0700

    fix

--- src/main/java/org/example/MainMain.java
+++ src/main/java/org/example/MainMain.java
@@ -1,7 +1,7 @@
 package org.example;
 
 
-public class Pukikaki {
+public class MainMain {
     public static void main(String[] args) {
 //        Button btn = new Button();
 //        btn.click();
